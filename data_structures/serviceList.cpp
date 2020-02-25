fb70ee58606ca46c7616a7132bcd2ae8977187b3
tgomez22 <gomez22@pdx.edu>
2020-02-25 09:35:51 -0800

    I removed a duplicate destructor and remove function. Deleted the weird comments git added during merging

--- data_structures/serviceList.cpp
+++ data_structures/serviceList.cpp
@@ -1,10 +1,5 @@
 #include "serviceList.h"
-<<<<<<< HEAD
 
-serviceList::serviceList()
-{
-	table = new sNode[SIZE];
-=======
 #include "service.h"
 #include "entity.h"
 #include "person.h"
@@ -14,10 +9,10 @@ serviceList::serviceList()
 const int ID = 10;
 const int SIZE = 101;
 const int MULTBY = 97;
+
 serviceList::serviceList()
 {
 	table = new serviceNode*[SIZE];
->>>>>>> master
 
 	for(int i = 0; i < SIZE; ++i)
 	{
@@ -27,34 +22,6 @@ serviceList::serviceList()
 	table -= SIZE;
 }
 
-<<<<<<< HEAD
-//UNDER CONSTRUCTION
-serviceList::~serviceList()
-{
-  serviceNode ** start = table;
-  for(int i = 0; i < 0; ++i)
-  {
-    destroyService(start[i]);
-  }
-}
-
-void destroyService(serviceNode *& to_destroy)
-{
-  if(!to_destroy->next)
-  {
-    to_destroy->head.~service();
-    delete to_destroy;
-    to_destroy = NULL;
-    return;
-  }
-  if(!to_destroy)
-    return;
-  to_destroy->head.~service();
-  deleteServices(to_destroy->head);
-  delete to_destroy;
-  to_destroy = NULL;
-  return;
-=======
 serviceList::~serviceList()
 {
     for(int i = 0; i < SIZE; ++i)
@@ -75,8 +42,10 @@ void serviceList::removeNodes(serviceNode *& toRemove)
         return;
 
     removeNodes(toRemove->next);
+    
+    if(toRemove->aPerson)
+        delete toRemove->aPerson;
 
-    delete toRemove->aPerson;
     toRemove->aPerson = NULL;
     removeServices(toRemove->head);
     toRemove->head = NULL;
@@ -96,7 +65,6 @@ void serviceList::removeServices(service *& toRemove)
 
     return;
 
->>>>>>> master
 }
 
 int serviceList::memberFunctions()
@@ -104,8 +72,6 @@ int serviceList::memberFunctions()
 
 }
 
-<<<<<<< HEAD
-=======
 int serviceList::addPerson(const person & toAdd)
 {
 
@@ -188,7 +154,6 @@ int serviceList::addPerson(const person & toAdd)
 
 }
 
->>>>>>> master
 int serviceList::getKey(const int toUse)
 {
 	int key = toUse * MULTBY;
@@ -200,45 +165,14 @@ int serviceList::getKey(const int toUse)
 	return key;
 }
 
-
-///check logic again???
-<<<<<<< HEAD
-bool serviceList::addService(const service & toAdd)
-{
-	int index = getKey(toAdd.getIdValue());
-
-	if(table[index] == NULL)
-	{
-		table[index] = new sNode;
-		table[index]->memNum.addId(toAdd);;
-		table[index]->head = new service(toAdd);
-		table[index]->next = NULL;
-
-		return true;
-	}
-
-	else
-	{
-		sNode * temp = table[index];
-
-		while(temp && temp->memNum.compare(toAdd)!= 0)
-		{
-			temp = temp->next;
-		}
-
-=======
 bool serviceList::addService(const class service & toAdd)
 {
 	int index = getKey(toAdd.getIdValue());
 
+    //no person to associate service with
 	if(!table[index])
 	{
-		table[index] = new serviceNode;
-		table[index]->aPerson->addId(toAdd);
-		table[index]->head = new service(toAdd);
-		table[index]->next = nullptr;
-
-		return true;
+		return false;
 	}
 
 	else
@@ -250,28 +184,20 @@ bool serviceList::addService(const class service & toAdd)
 			temp = temp->next;
 		}
 
->>>>>>> master
         //fell off list, member doesn't exist
 		if(!temp)
 		{
 			//must be an existing member to add a service.
 			return false;
 		}
-<<<<<<< HEAD
 
-=======
 
->>>>>>> master
         //found member, adding service
 		else
 		{
 			service * headHolder = temp->head;
 			temp->head = new service(toAdd);
-<<<<<<< HEAD
-			temp->head.to_next() = headHolder;
-=======
 			temp->head->toNext() = headHolder;
->>>>>>> master
 			return true;
 
 		}
