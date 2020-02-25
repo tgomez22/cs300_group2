51f4958f718fe2260fe104e972e754f2d380caa5
shawnspears <shspears@pdx.edu>
2020-02-24 19:33:04 -0800

    Add entity.h

--- model/entity.h
+++ model/entity.h
@@ -1,28 +1,47 @@
-#ifndef __ENTITY_H
-#define __ENTITY_H
 //CS300 group #2
 //
 //Worked on by Tristan Gomez (Started 2/5/2020)
-
-#include <iostream>
-
-using id_num = unsigned int;
-const int ID = 9;
-
+#ifndef ENTITY_H
+#define ENTITY_H
+#include "tString.h"
 class entity
 {
 	public:
-    	    entity();
-	    entity(const entity & toAdd);
-	    ~entity();
-	    bool getIdFromTerm();
-            bool checkIdFromScan(char * scanID);
-	    void setId(id_num id);
-	    int compare(const entity & toCompare)const;
-	    id_num getIdValue()const;
+        //default constructor, sets null values
+		entity();
+
+        //initializes to arg values
+		entity(const entity & toAdd);
+		~entity();
+
+        //terminal people fctn. prompt user to directly enter their memId to store.
+		bool addId();
+
+        //copies arg into data member
+		bool addId(const class tString & toAdd);
+        bool addId(const entity & toAdd);
+		bool addId(char * toAdd);
+
+
+		void display()const;
+
+        //returns true if matches arg.
+		bool isMatch(const entity & toSee)const;
+
+        //returns 1,2,3 depending on user type. if not value user, returns 0;
+        int getFirstIndex()const;
+
+        //returns sum of ascii values of stored data. Used in hashing fctn.
+        int getIdValue()const;
+
+        //strcmps arg against stored data. returns 0 if match
+        int compare(const entity & toCompare)const;
+        int compare(char * toCompare)const;
+
+        //FILE IO PEOPLE!!! prototype to write out to file.
+        bool writeOut();
 	protected:
-	    char* isIDValid(char*);
-	    id_num id;
+		tString memId;
 };
 
 #endif
