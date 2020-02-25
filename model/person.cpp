d6334e1daed3ee2dd7f6d11d467d77dc3d9527dd
shawnspears <shspears@pdx.edu>
2020-02-24 19:35:19 -0800

    Add person.cpp

--- model/person.cpp
+++ model/person.cpp
@@ -1,21 +1,31 @@
 #include "person.h"
+#include "entity.h"
 
 person::person():entity()
 {
-
 }
 
-person::~person() {
-
+person::~person()
+{
 }
 
 person::person(const person & toAdd):entity(toAdd)
-{	
-	name.assign(toAdd.name);
-	address.assign(toAdd.address);
-	city.assign(toAdd.city);
-	state.assign(toAdd.state);
-	zip.assign(toAdd.zip);
-	//todo copy service list
+{
+	name.add(toAdd.name);
+	address.add(toAdd.address);
+	city.add(toAdd.city);
+	state.add(toAdd.state);
+	zip.add(toAdd.zip);
 }
 
+void person::showInfo()const
+{
+    entity::display();
+    name.display();
+    address.display();
+    city.display();
+    state.display();
+    zip.display();
+
+    return;
+}
