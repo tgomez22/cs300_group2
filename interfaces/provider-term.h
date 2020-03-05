1288c2c7d6dacb2a7b50314bfe5f27264d16e016
Mikayla Maki <mtrenton@pdx.edu>
2020-03-04 19:03:11 -0800

    Added 2 comments

--- interfaces/provider-term.h
+++ interfaces/provider-term.h
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+//Begin all provider terminal stuff here. When this function returns, the app will pop back up to the 'choose terminal' interface.
+//Use exitFunction() to quit
 void providerTerm(string id_num);
 
 #endif
