503132942d33636fa779ad06e2dd933ba8017edd
shawnspears <shspears@pdx.edu>
2020-03-06 14:43:48 -0800

    Test for populating personList upon program execution PASSES

--- data_structures/test/datacenter_test.cpp
+++ data_structures/test/datacenter_test.cpp
@@ -5,7 +5,6 @@ using namespace std;
 void datacenterTest() {
 	cout << "\n === COMMENCING TESTS === \n" << endl;
 	personList table;
-	table.readIn();
 	table.displayStored();
 	cout << "\n ===== END OF TESTS ===== \n" << endl;
 }
