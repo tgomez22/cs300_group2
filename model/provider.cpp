7a73d375bd64bbbc4e24b44c6854395f6bafc6be
shawnspears <shspears@pdx.edu>
2020-03-06 16:50:27 -0800

    Add provider writeOut function

--- model/provider.cpp
+++ model/provider.cpp
@@ -30,7 +30,32 @@ bool provider::readIn()
 //Here's your prototype Shawn!
 void provider::writeOut()
 {
-
+	ofstream myFile;
+	myFile.open("data/provider.txt", ios::app);
+	if(myFile)
+	{
+		char * writeMemId = memId.getString();
+		char * writeName = name.getString();
+		char * writeAddress = address.getString();
+		char * writeCity = city.getString();
+		char * writeState = state.getString();
+		char * writeZip = zip.getString();
+		json toWrite;
+		toWrite["memId"] = fileMemId;
+		toWrite["name"] = fileName;
+		toWrite["address"] = fileAddress;
+		toWrite["city"] = fileCity;
+		toWrite["zip"] = fileZip;
+		toWrite["consultNum"] = consultNum;
+		toWrite["weeklyFee"] = weeklyFee;
+		myFile << toWrite;
+		myFile.close();
+		system("openssl aes-256-cbc -salt -pbkdf2 -in data/provider.txt -out data/providerEncrypted.dat -pass pass:password"); //encryption
+		delete []writeMemId, delete []writeName, delete []writeAddress, delete []writeCity, delete []writeState, delete []writeZip;
+	}
+	else
+		cout << "Unable to open file." << endl;
+	return;
 }
 
 
