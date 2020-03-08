8e729584faa002ba8ffac8712a0e67c8a58983cf
shawnspears <shspears@pdx.edu>
2020-03-08 16:48:16 -0700

    Add readIn to constructor to populate table

--- model/provider.cpp
+++ model/provider.cpp
@@ -113,7 +113,7 @@ bool provider::suspendMember()
     return false;
 }
 
-void provider::addInfo(const char * addID, const char * addName, const char * addAddress, const char * addCity, const char * addState, const char * addZip, int addConsultNum, flo    at addWeeklyFee)
+void provider::addInfo(const char * addID, const char * addName, const char * addAddress, const char * addCity, const char * addState, const char * addZip, int addConsultNum, float addWeeklyFee)
 {
     person::addInfo(addID, addName, addAddress, addCity, addState, addZip);
     consultNum = addConsultNum;
