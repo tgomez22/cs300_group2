db1ec8f6f90e31ffe32c0f29b93f533eaeea9c0b
shawnspears <shspears@pdx.edu>
2020-03-10 15:06:52 -0700

    Add function to display stored data in serviceList

--- data_structures/test/datacenter_test.cpp
+++ data_structures/test/datacenter_test.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include "../personList.h"
+#include "../serviceList.h"
 using namespace std;
 
 void datacenterTest() {
 	cout << "\n === COMMENCING TESTS === \n" << endl;
-	personList table;
-	table.displayStored();
+	cout << "\nPERSON LIST (AUTH):" << endl;
+	personList ids;
+	ids.displayStored();
+	cout << "\nSERVICE LIST:" << endl;
+	serviceList services;
+	services.displayStored();
 	cout << "\n ===== END OF TESTS ===== \n" << endl;
 }
 
